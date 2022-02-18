8dc87c3709700a198b164e8653acf8c03aea90c0
Sunil Green <green380@purdue.edu>
2022-02-18 05:20:12 -0500

    More documentation for manual sampling in seeds.py

--- Data/sampling/seeds.py
+++ Data/sampling/seeds.py
@@ -18,20 +18,33 @@ def getPoints(seed, n, start, stop):
 
     random.seed(seed)
     points = random.sample(point_list, n)
+    points.sort()
     print("######### Random Sample #########")
     print(points)
     print("######### End Random Sample #########")
     return points
 
 '''
-The samples below are used for the manual identification of data.
+The samples below were used for the manual identification of data.
 Once issues with data cleaning are resolved this will be automated.
 Only used for an intitial run. Because of the under-represenation of
 pork in the combined data (assuming 10% pork per CAGW), samples
 are taken from each seperate part of the data.
 '''
-getPoints(100, 27, 0, 12816) #Getting negative samples
-getPoints(100, 3, 12816, 13233) #Getting positive samples
+#getPoints(100, 28, 0, 12816) #Getting negative samples
+#getPoints(100, 3, 12816, 13233) #Getting positive samples
+'''
+Some samples were changed due to data not fitting, or already in use in training
+The rule was to add 7 to the id (index) and if that sample was not in use and could 
+fit the model it was used. Some samples needed multiple +7's applied to find a usuable row
+
+Negative samples: [806, 1313, 1801, 1987, 2386, 2863, 2923, 3340, 3350, 3763, 4315, 5069, 5494, 5730, 6439, 7102, 7453, 7477, 7528, 8304, 8731, 10608, 10794, 11559, 11992, 12070, 12583, 12628]
+Positive samples: [12897, 13048, 13051]
+
+Any differences with a seed value of 100 are a result of the +7 rule
+'''
+
+
 
 #rawData = pd.read_excel("./TCSEarmarks.xlsx")
 # print(rawData.columns.values)
